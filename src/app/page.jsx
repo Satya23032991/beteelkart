821a2bebe7c2e45485c3d58644c6f36e72230fef
satya23032991 <satya.parija@indianpaansamrat.com>
2024-06-26 11:32:53 +0530

    new one

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Welcome to IndianPaanSamrat</h1>
+      <p>Exciting Products and New Offers coming Soon stay tuned</p> 
+    </main>
+  );
+}
