c7f4e1c06d879eeca3a3ddbaea4d1ae4a3927ae0
satya23032991 <satya.parija@indianpaansamrat.com>
2024-07-03 19:06:32 +0530

    undefined window-test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,12 @@
 
+//import dynamic from 'next/dynamic';
+import Herosection from '@/components/HeroSection';
+import Aboutsection from '@/components/AboutSection';
+  import ProductSection from '@/components/ProductsSection';
 
- import Herosection from '@/components/HeroSection';
- import Aboutsection from '@/components/AboutSection';
- import ProductSection from '@/components/ProductsSection';
+// const DynamicProduct = dynamic(() => import('@/components/ProductsSection'))
+
+
 
 
 
@@ -15,6 +19,7 @@ export default function Home() {
     <Herosection/>
     <Aboutsection/>
     <ProductSection/>
+    {/* <DynamicProduct/> */}
       <p>Exciting Products and New Offers coming Soon stay tuned</p> 
       <p>....Application Under Development</p>
     </main>
