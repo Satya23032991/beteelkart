bd6c6dad24dda3913f777dacba6bca9f62d15f0e
satya23032991 <satya.parija@indianpaansamrat.com>
2024-07-15 17:18:29 +0530

    Sub-Category Added

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -3,11 +3,11 @@
 
 const Footersection = () => {
     return (
-        <div className="w-full bg-[#091e3e] py-10 mt-5">
+        <div className="w-full bg-[#e3ea88] py-10 mt-0">
             <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-5 gap-10 px-4 py-5">
                 <div>
-                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-white">About Us</h4>
-                    <ul className="text-white">
+                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-[#375736]">About Us</h4>
+                    <ul className="text-[#375736]">
                         <li><Link href="/">Coming Soon</Link></li>
                         <li><Link href="/">Coming Soon</Link></li>
                         <li><Link href="/">Coming Soon</Link></li>
@@ -17,8 +17,8 @@ const Footersection = () => {
                 </div>     
 
                 <div>
-                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-white">Careers</h4>
-                    <ul className="text-white">
+                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-[#375736]">Careers</h4>
+                    <ul className="text-[#375736]">
                         <li><Link href="/">Coming Soon</Link></li>
                         <li><Link href="/">Coming Soon</Link></li>
                         <li><Link href="/">Coming Soon</Link></li>
@@ -28,7 +28,7 @@ const Footersection = () => {
                 </div>
 
                 <div>
-                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-white">Contact Us</h4>
+                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-[#375736]">Contact Us</h4>
                 </div>
             </div>
         </div>
