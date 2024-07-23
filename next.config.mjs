024ea677dc5c8baf17fc1eff395be8b58e40f2e1
satya23032991 <satya.parija@indianpaansamrat.com>
2024-07-23 16:07:33 +0530

    youtube window added in the traditional paan

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "frame-src 'self' https://www.youtube.com/ https://m.youtube.com/;",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
