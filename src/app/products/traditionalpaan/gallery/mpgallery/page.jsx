5baea1a274b3734a9627f48c42ca7f1dbe6a6929
satya23032991 <satya.parija@indianpaansamrat.com>
2024-07-24 16:40:11 +0530

    patch for the typosand navigations created for buttons

---
+++ src/app/products/traditionalpaan/gallery/mpgallery/page.jsx
@@ -0,0 +1,8 @@
+
+ const Mpgallery = () =>{
+    return(
+        <h1>Meetha Paan Gallery</h1>
+    )
+ }
+
+ export default Mpgallery;
---
+++ src/app/products/traditionalpaan/gallery/spgallery/page.jsx
@@ -0,0 +1,9 @@
+
+
+ const Spgallery = () => {
+    return(
+        <h1>SaadaPaan Gallery</h1>
+    )
+ }
+
+ export default Spgallery;
