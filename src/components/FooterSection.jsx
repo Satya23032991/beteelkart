fe2ad7b651d62abce30021701eb7ee07eb076b34
satya23032991 <satya.parija@indianpaansamrat.com>
2024-08-07 04:07:34 +0530

    footer-section-development

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,12 +1,29 @@
 
+ "use client"
+
  import Link from "next/link";
+ import Image from 'next/image';
+ import { library } from '@fortawesome/fontawesome-svg-core';
+ import { faFacebookF, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+ import Document, { Html, Head, Main, NextScript } from 'next/document';
+//  import instagram from '../images/insta.png';
+
+ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+ 
+ library.add(faFacebookF, faInstagram, faYoutube, faCoffee);
+
 
 const Footersection = () => {
     return (
         <div className="w-full bg-[#e3ea88] py-10 mt-0">
-            <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-5 text-center gap-10 px-4 py-5">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-6 text-center gap-10 px-4 py-5">
                 <div>
-                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-[#375736]">About Us</h4>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        About Us
+                        </h4>
                     <ul className="text-[#375736]">
                         <li><Link href="/">Our Journey</Link></li>
                         <li><Link href="/">Our Vision</Link></li>
@@ -14,21 +31,113 @@ const Footersection = () => {
                         <li><Link href="/">Our Motivation</Link></li>
                         <li><Link href="/">Our Goal</Link></li>
                     </ul>
-                </div>     
+                </div> 
+
+                 <div>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Our Products
+                        </h4>
+                    <ul className="text-[#375736]">
+                        <li><Link href="/">Traditional</Link></li>
+                        <li><Link href="/">Ice-Burst</Link></li>
+                        <li><Link href="/">Special</Link></li>
+                        <li><Link href="/">Coated</Link></li>
+                        <li><Link href="/">Combo</Link></li>
+                    </ul> 
+                </div>   
+
+
 
                 <div>
-                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-[#375736]">Contact Us</h4>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Contact Us
+                        </h4>
                     <ul className="text-[#375736]">
-                        <li><Link href="/">Our Branches</Link></li>
+                        <li><Link href="/"> Through Phone No</Link></li>
                         <li><Link href="/">Our Officials</Link></li>
-                        <li><Link href="/">Our Telephone</Link></li>
+                        <li><Link href="/">Our Customer Care</Link></li>
                         <li><Link href="/">Our Email</Link></li>
                         <li><Link href="/">Our Head-Office</Link></li>
                     </ul>       
                 </div>
 
                 <div>
-                    <h4 className="mt-20 md:mt-0 text-1xl md:text-1xl font-bold text-[#375736]">Careers</h4>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Offers
+                        </h4>
+                    <ul className="text-[#375736]">
+                        <li><Link href="/"> Monthly Jackpot</Link></li>
+                        <li><Link href="/">Membership Offers</Link></li>
+                        <li><Link href="/"> Festival Offers</Link></li>
+                        <li><Link href="/">Offer Of the Day</Link></li>
+                        <li><Link href="/">Return Offers</Link></li>
+                    </ul>       
+                </div>
+
+                <div>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Quick Links
+                        </h4>
+                    <ul className="text-[#375736]">
+                        <li><Link href="/"> FAQs</Link></li>
+                        <li><Link href="/">Delivery Instructions</Link></li>
+                        <li><Link href="/">Replacement Policy</Link></li>
+                        <li><Link href="/">Cancellation Policy</Link></li>
+                        <li><Link href="/">Exchange Policy</Link></li>
+                    </ul>       
+                </div>
+
+                <div>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Our Newsletters
+                        </h4>
+                    <ul className="text-[#375736]">
+                        <li><Link href="/"> Weekly Newsletter </Link></li>
+                        <li><Link href="/">Monthly Newsletter</Link></li>
+                        <li><Link href="/">Quarterly Newsletter</Link></li>
+                        <li><Link href="/">Half-Yearly Newsletter</Link></li>
+                        <li><Link href="/">Yearly Newsletter</Link></li>
+                    </ul>       
+                </div>
+
+                <div>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Testimonials
+                        </h4>
+                    <ul className="text-[#375736]">
+                        <li><Link href="/"> Most Popular </Link></li>
+                        <li><Link href="/">Recent</Link></li>
+                        <li><Link href="/">Relevant</Link></li>
+                        <li><Link href="/">Famous Personalities</Link></li>
+                        <li><Link href="/">All</Link></li>
+                    </ul>       
+                </div>
+
+                <div>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Store Locator
+                        </h4>
+                    <ul className="text-[#375736]">
+                        <li><Link href="/"> Locate By Zip Code </Link></li>
+                        <li><Link href="/">Locate By City</Link></li>
+                        <li><Link href="/">Locate By Landmark</Link></li>
+                        <li><Link href="/">Locate By State</Link></li>
+                        <li><Link href="/">Locate Nearest to you</Link></li>
+                    </ul>       
+                </div>
+
+                <div>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Careers
+                        </h4>
                     <ul className="text-[#375736]">
                         <li><Link href="/">Business</Link></li>
                         <li><Link href="/">Technical</Link></li>
@@ -37,6 +146,45 @@ const Footersection = () => {
                         <li><Link href="/">Salesman</Link></li>
                     </ul> 
                 </div>
+
+                <div>
+                    <h4 className="mt-20 md:mt-0 text-1xl
+                     md:text-1xl font-bold text-[#da371d]">
+                        Legal
+                        </h4>
+                    <ul className="text-[#375736]">
+                        <li><Link href="/"> Privacy Policies </Link></li>
+                        <li><Link href="/">Terms and Conditions</Link></li>
+                        <li><Link href="/">Legal Certificates</Link></li>
+                        <li><Link href="/">Awards </Link></li>
+                        <li><Link href="/">Product Certificates</Link></li>
+                    </ul>       
+                </div>
+
+                <div className="col-span-2 flex justify-center items-center bg-transparent  border border-transparent">
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faFacebookF} className="text-[#375736] text-4xl p-5" />
+                    </a>
+
+                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagram} className="text-[#375736] text-4xl p-5" />
+                    </a>
+
+{/* <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+    <img src="/images/insta.png" alt="Instagram" className="h-8 w-8 p-5" />
+</a> */}
+
+
+                    {/* <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faYoutube} className="text-[#375736] text-4xl" />
+                    </a> */}
+
+                    <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png" alt="YouTube" className="h-8 w-8 " />
+                    </a>
+
+                </div>
+
             </div>
         </div>
     )
