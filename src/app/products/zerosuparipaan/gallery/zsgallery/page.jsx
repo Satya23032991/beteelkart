936ce358055860ce6ac979c451fd623df3e2075b
satya23032991 <satya.parija@indianpaansamrat.com>
2024-08-14 11:59:19 +0530

    images-changed-for-zspaan

--- src/app/products/zerosuparipaan/gallery/zsgallery/page.jsx
+++ src/app/products/zerosuparipaan/gallery/zsgallery/page.jsx
@@ -1,15 +1,15 @@
 import styles from './zsstyle.module.css';
 import Image from 'next/image';
-import image1 from '/src/images/kiddiepaan.jpg';
-import image2 from '/src/images/OUR_TIMETRAVEL.jpg';
-import image3 from '/src/images/coated_paan_theory.jpg';
-import image4 from '/src/images/meetha-paan-gallery/Baba_dilbahar.jpg';
-import image5 from '/src/images/meetha-paan-gallery/Banarasi_meetha.jpg';
-import image6 from '/src/images/meetha-paan-gallery/Calcutta_meetha.jpg';
-import image7 from '/src/images/meetha-paan-gallery/Maghai_twins.jpg';
-import image8 from '/src/images/meetha-paan-gallery/Meenakshi_meetha.jpg';
-import image9 from '/src/images/meetha-paan-gallery/Navratna_meetha.jpg';
-import image10 from '/src/images/coated_paan_theory.jpg';
+import image1 from '/src/images/one.jpg';
+import image2 from '/src/images/two.jpg';
+import image3 from '/src/images/three.jpg';
+import image4 from '/src/images/four.jpg';
+import image5 from '/src/images/five.jpg';
+import image6 from '/src/images/six.jpg';
+import image7 from '/src/images/seven.jpg';
+import image8 from '/src/images/eight.jpg';
+import image9 from '/src/images/nine.jpg';
+import image10 from '/src/images/ten.jpg';
 
 const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10];
 
