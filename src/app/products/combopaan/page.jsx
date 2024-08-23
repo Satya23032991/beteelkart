d0948cc52d22aaa198d74d6cd17fcebd96dd093e
satya23032991 <satya.parija@indianpaansamrat.com>
2024-08-23 19:16:54 +0530

    aws-error

--- src/app/products/combopaan/page.jsx
+++ src/app/products/combopaan/page.jsx
@@ -13,9 +13,9 @@
         Immerse yourself in an extraordinary fusion of flavors with our enchanting Combo-Paan offering.
          Here, we elevate tradition with a delightful twist, creating a symphony of taste that captures both indulgence and nostalgia.
           Each paan is lovingly crafted to perfection, blending the finest betel leaves with aromatic fillings, rich spices,
-           and refreshing hints of sweetness that are synonymous with our age-old tradition. But the journey doesn't stop there.
+           and refreshing hints of sweetness that are synonymous with our age-old tradition. But the journey doesn&apos;t stop there.
 
-         To complement your paan experience, we’ve added a luxurious touch with a selection of delectable chocolates 
+         To complement your paan experience, we&apos;ve added a luxurious touch with a selection of delectable chocolates 
          that add a layer of indulgence to this classic treat. These chocolates, made from the finest ingredients, 
          serve as the perfect pairing, offering a rich, velvety contrast to the vibrant and bold flavors of our paan.
           Imagine savoring the smooth creaminess of exquisite chocolate that melts in your mouth, 
@@ -23,9 +23,9 @@
           and elevate your senses.
 
          This unique combination embodies the essence of celebration, tradition, and indulgence. 
-         Whether you're enjoying it as a moment of self-pampering or sharing it with friends and family,
-          the Combo-Paan offers a multisensory experience that is both satisfying and memorable. It's more than just a treat; 
-          it’s a celebration of taste and culture, thoughtfully designed for those who appreciate the finer things in life.
+         Whether you&apos;re enjoying it as a moment of self-pampering or sharing it with friends and family,
+          the Combo-Paan offers a multisensory experience that is both satisfying and memorable. It&apos;s more than just a treat; 
+          it&apos;s a celebration of taste and culture, thoughtfully designed for those who appreciate the finer things in life.
            Let the harmony of flavors enchant you and take you on a journey of taste like no other.
         </p>
         <Link href="traditionalpaan/gallery/spgallery" passHref>
